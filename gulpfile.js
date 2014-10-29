637ec9f03856faae98a5e8159305b671cb8caea8
Dmitry Shvalyov <dsh-creative+subscription@yandex.ru>
2014-10-29 22:20:17 +0300

    Jade template engine

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
-/* global require, console */
+/* global require, process, console */
 'use strict';
 
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')(),
+    template = 'template',
     app = 'app';
+
+gulp.task('template', function() {
+	gulp.src('./' + template + '/index.jade')
+	.pipe($.jade())
+	.on('error', function(error) {
+		console.log(error);
+		this.end();
+	})
+	.pipe(gulp.dest('./' + app + '/'));
+});
 
 gulp.task('server', function(next) {
 	var connect = require('connect'),
@@ -20,6 +31,7 @@ gulp.task('server', function(next) {
 gulp.task('watch', ['server'], function() {
 	var server = $.livereload();
 
+	gulp.watch([template + '/**'], [template]);
 	gulp.watch(app + '/**').on('change', function(file) {
 		server.changed(file.path);
 	});
